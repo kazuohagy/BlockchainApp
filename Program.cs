fedb3a0a15d1e12ab9d47fb1f6b731c4e6c234fe
Gustavo Kazuo <kazucalado@gmail.com>
2024-11-01 17:32:17 -0300

    [Program.cs] fix

--- Program.cs
+++ Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Blockchain phiCoin = new Blockchain();
+        phiCoin.AddBlock(new Block(1, DateTime.Now, phiCoin.Chain[^1].Hash, "Block 1 Data"));
+        phiCoin.AddBlock(new Block(2, DateTime.Now, phiCoin.Chain[^1].Hash, "Block 2 Data"));
+
+        foreach(Block block in phiCoin.Chain){
+            Console.WriteLine($"Index: {block.Index}");
+            Console.WriteLine($"Timestamp: {block.TimeStamp}");
+            Console.WriteLine($"Previous Hash: {block.PreviousHash}");
+            Console.WriteLine($"Hash: {block.Hash}");
+            Console.WriteLine($"Data: {block.Data}");
+            Console.WriteLine();
+        }
+    }
+}
