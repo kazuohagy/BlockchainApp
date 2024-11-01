3dfcffb3505db376b924b3a79064397a4b17802f
Gustavo Kazuo <kazucalado@gmail.com>
2024-11-01 14:10:13 -0300

    initial commit

---
+++ Block.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Security.Cryptography;
+using System.Text;
+
+public class Block{
+    public int Index { get; set; }
+    public DateTime TimeStamp { get; set; }
+    public string PreviousHash { get; set; }
+    public string Hash { get; set; }
+    public string Data { get; set; }
+    public int Nonce { get; set; }
+
+    public Block(DateTime timeStamp, string previousHash, string data){
+        Index = 0;
+        TimeStamp = timeStamp;
+        PreviousHash = previousHash;
+        Data = data;
+        Hash = CalculateHash();
+        Nonce = 0;
+    }
+
+    public string CalculateHash(){
+        SHA256 sha256 = SHA256.Create();
+        byte[] inputBytes = Encoding.ASCII.GetBytes($"{TimeStamp}-{PreviousHash ?? ""}-{Data}-{Nonce}");
+        byte[] outputBytes = sha256.ComputeHash(inputBytes);
+        return Convert.ToBase64String(outputBytes);
+    }
+
+    public void Mine(int difficulty){
+        var leadingZeros = new string('0', difficulty);
+        while(Hash == null || Hash.Substring(0, difficulty) != leadingZeros){
+            Nonce++;
+            Hash = CalculateHash();
+        }
+    }
+}
